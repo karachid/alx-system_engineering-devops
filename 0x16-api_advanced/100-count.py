b93c345d948601940f93064ba0846a4d8d47ae7c
Rachid KABBAB <r.kabbab@gmail.com>
2024-02-13 15:05:54 -0800

    3 Count it!

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Function that queries Reddit API
+"""
+
+import requests
+
+after = None
+count = 0
+
+
+def count_words(subreddit, word_list):
+    """
+    It counts the number of occurence of a given keyword
+    from reddit api titles
+
+    Args:
+        subreddit (string): subreddit to fetch
+        word_list (list): list of keyword to count for matche
+    """
+    global after
+    global count
+    
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla"
+    }
+    params = {
+        "after": after,
+        "count": count,
+    }
+    req = requests.get(url, params=params, headers=headers,
+                       allow_redirects=False)
+    if req.status_code == 200:
+        res = req.json()
+        titles = [c["data"]["title"] for c in res["data"]["children"]]
+        for title in titles:
+            print(title)
